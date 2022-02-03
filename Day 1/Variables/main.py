83fb72cd6efeaef20bdd47eca3667699590c1f5e
jarvisclouds <83707273+jarvisclouds@users.noreply.github.com>
2022-02-03 17:53:25 +0200

    Add files via upload

---
+++ Day 1/Variables/main.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+olda = a
+oldb = b
+a = oldb
+b = olda
+
+# OR
+# olda = a
+# a = b
+# b = olda
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
