83c011e7d3af459247f863435219a7da548fe6ad
jarvisclouds <83707273+jarvisclouds@users.noreply.github.com>
2022-02-03 23:10:03 +0200

    Update main.py

--- Day 2/Day 2 Project - Tip Calculator/main.py
+++ Day 2/Day 2 Project - Tip Calculator/main.py
@@ -3,6 +3,6 @@ bill = float(input("What was the total bill?\n"))
 perc = float(input("What percentage tip would you like to give? (10, 12, or 15?)\n"))*0.01
 peeps = int(input("How many people are going to split the bill?\n"))
 
-each = round((bill+(bill*perc))/peeps, 2)
+each = "{:.2f}".format(round((bill+(bill*perc))/peeps, 2))
 
 print(f"Each person should pay: {each}")
